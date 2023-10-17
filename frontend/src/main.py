61e4b877ede66bf6623c30d1dd0a5fa84aa33784
Arne van Iterson <arne@arnweb.nl>
2023-10-17 23:39:46 +0200

    Updated MCU output to JSON format

--- frontend/src/main.py
+++ frontend/src/main.py
@@ -1,11 +1,26 @@
 import serial
+import json
 
 # Replace 'COM7' with the name of your serial port
-ser = serial.Serial('COM7', 9600)
+ser = serial.Serial('COM10', 9600)
 
-while True:
-    # Read a line of data from the serial port
-    data = ser.readline().decode().strip()
+try:
+    while True:
+        # Read a line of data from the serial port
+        data = ser.readline().decode().strip()
     
-    # Print the data to the console
-    print(data)
+        # Print the data to the console
+        res = json.loads(data)
+        
+        print(F"Value: {res['digits']}")
+        
+        if res['polarity'] == 1: 
+            print("Polarity: Positive")
+        else:
+            print("Polarity: Negative")
+            
+        if res['overload'] == 1:
+            print("Overload warning!")
+except KeyboardInterrupt:
+    ser.close()
+    exit()
--- src/main.cpp
+++ src/main.cpp
@@ -1,22 +1,22 @@
 #include <Arduino.h>
 
 // Pin definitions
-#define FL8000A_MSD       14  // First digit 1/0
-#define FL8000A_2MSD_8    2  // Second digit 8/4/2/1
-#define FL8000A_2MSD_4    3
-#define FL8000A_2MSD_2    4
-#define FL8000A_2MSD_1    5
-#define FL8000A_3MSD_8    6  // Third digit 8/4/2/1
-#define FL8000A_3MSD_4    7
-#define FL8000A_3MSD_2    8
-#define FL8000A_3MSD_1    9
-#define FL8000A_LSD_8     10  // Last digit 8/4/2/1
-#define FL8000A_LSD_4     11
-#define FL8000A_LSD_2     12
-#define FL8000A_LSD_1     13
-#define FL8000A_POLARITY  15  // Polarity 1/0
-#define FL8000A_OVERLOAD  17  // Display overload warning 1/0
-#define FL8000A_BUSY      16  // Busy signal 1/0
+#define FL8000A_MSD 14   // First digit 1/0
+#define FL8000A_2MSD_8 2 // Second digit 8/4/2/1
+#define FL8000A_2MSD_4 3
+#define FL8000A_2MSD_2 4
+#define FL8000A_2MSD_1 5
+#define FL8000A_3MSD_8 6 // Third digit 8/4/2/1
+#define FL8000A_3MSD_4 7
+#define FL8000A_3MSD_2 8
+#define FL8000A_3MSD_1 9
+#define FL8000A_LSD_8 10 // Last digit 8/4/2/1
+#define FL8000A_LSD_4 11
+#define FL8000A_LSD_2 12
+#define FL8000A_LSD_1 13
+#define FL8000A_POLARITY 15 // Polarity 1/0
+#define FL8000A_OVERLOAD 17 // Display overload warning 1/0
+#define FL8000A_BUSY 16     // Busy signal 1/0
 
 char busy = 0;
 char busy_last = 0;
@@ -54,13 +54,13 @@ void update()
   uint8_t overload = digitalRead(FL8000A_OVERLOAD);
 
   // Print the values
-  Serial.println("Reading:");
-  Serial.print(msd);
-  Serial.print(msd2);
-  Serial.print(msd3);
-  Serial.println(lsd);
-  Serial.println(polarity);
-  Serial.println(overload);
+  char buffer[64];
+  sprintf(buffer, "{\"digits\":[%d, %d, %d, %d], "
+                  "\"polarity\":%d, "
+                  "\"overload\":%d}",
+                  msd, msd2, msd3, lsd,
+                  polarity, overload);
+  Serial.println(buffer);
 }
 
 void loop()
